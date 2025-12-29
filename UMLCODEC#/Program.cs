d10b7f1a3b71ca70e3ca6909047b2efc63057f17
AHMJ2005 <abojyefl@gmail.com>
2025-12-29 21:53:29 +0300

    initial c# project

---
+++ UMLCODEC#/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace UMLCODEC_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hi!");
+            Console.WriteLine("Hello, World!");
+
+        }
+    }
+}
